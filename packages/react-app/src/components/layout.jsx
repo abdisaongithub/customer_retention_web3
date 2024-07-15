24bccc454bba84444c666269ca42d47e72281109
Abdisa Tsegaye <48585612+abdisaongithub@users.noreply.github.com>
2024-07-15 20:29:08 +0300

    ready for backend

--- packages/react-app/src/components/layout.jsx
+++ packages/react-app/src/components/layout.jsx
@@ -1,19 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./footer";
 import Header from "./header";
+import { useLocation } from "react-router-dom";
 
 const Layout = (props) => {
-    return (
-        <>
-            <div className="bg-gypsum overflow-hidden flex flex-col min-h-screen">
-                <Header />
-                <div className="py-0 max-w-7xl mx-0 space-y-8 sm:px-6 lg:px-8">
-                    <Outlet />
-                </div>
-                {/* <Footer /> */}
-            </div>
-        </>
-    );
+  const location = useLocation();
+  const { hash, pathname, search } = location;
+
+  return (
+    <>
+      <div className="bg-gypsum overflow-hidden flex flex-col min-h-screen">
+        {pathname.includes("profile") ? <></> : <Header />}
+
+        <div className="py-0 max-w-7xl mx-0 space-y-8 sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+        {/* <Footer /> */}
+      </div>
+    </>
+  );
 };
 
 export default Layout;
